4724c5d3f5e459da1abb08250270876fe93b95e5
Leon Schenk <leon_schenk@live.nl>
2016-07-09 16:19:24 +0000

    added kronos_config autoloader

---
+++ kronos_config.sh
@@ -0,0 +1,7 @@
+
+#
+echo "\$CONFIG['kronos_user'] = getenv('');" >> config.php
+echo "\$CONFIG['kronos_password'] = getenv('')" >> config.php
+echo "\$CONFIG['kronos_database'] = getenv('')" >> config.php
+echo "\$CONFIG['kronos_hostname'] = getenv('')" >> config.php
+
