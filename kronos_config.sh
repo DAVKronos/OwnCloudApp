2e526631b1246b176fef3d715804b1adf2b356ea
Leon Schenk <leon_schenk@live.nl>
2016-07-09 18:29:55 +0200

    Update kronos_config.sh

--- kronos_config.sh
+++ kronos_config.sh
@@ -1,6 +1,6 @@
 #!bin/env bash
-echo "\$CONFIG['kronos_user'] = getenv('KRONOS_USER');" >> config.php
-echo "\$CONFIG['kronos_password'] = getenv('KRONOS_PASSWORD')" >> config.php
-echo "\$CONFIG['kronos_database'] = getenv('KRONOS_DATABASE')" >> config.php
-echo "\$CONFIG['kronos_hostname'] = getenv('KRONOS_HOSTNAME')" >> config.php
+echo "\$CONFIG['kronos_user'] = getenv('KRONOS_USER');" >> /var/www/html/config/config.php
+echo "\$CONFIG['kronos_password'] = getenv('KRONOS_PASSWORD')" >> /var/www/html/config/config.php
+echo "\$CONFIG['kronos_database'] = getenv('KRONOS_DATABASE')" >> /var/www/html/config/config.php
+echo "\$CONFIG['kronos_hostname'] = getenv('KRONOS_HOSTNAME')" >> /var/www/html/config/config.php
 
